6245107a912abd41b9ded2d76f7eb4fd70c6a04a
Ajinkya862 <91425760+Ajinkya862@users.noreply.github.com>
2024-03-18 22:41:56 +0530

    Update1 videofinal.py

--- videofinal.py
+++ videofinal.py
@@ -1,4 +1,3 @@
-#updated till 
 # input video --> read_video  --> face_detection (MP)  ---> resize  ---> chunking  --->
 import torch
 import os
@@ -102,8 +101,6 @@ def process(frames):
 
 
 def prediction(data_loader, model_path='A:/Pycharm/newfinaldemo/PURE_TSCAN.pth', device='cpu'):
-    #data_loader = []
-    #data_loader.append(frames)
 
     predictions = dict()
     chunk_len=210
@@ -114,49 +111,12 @@ def prediction(data_loader, model_path='A:/Pycharm/newfinaldemo/PURE_TSCAN.pth',
     model = torch.nn.DataParallel(model, device_ids=list(range(1)))
 
     model.load_state_dict(torch.load(model_path, map_location=torch.device(device)))
-    #model.load_state_dict(torch.load(model_path))
     model = model.double()
     model = model.to(device)
     model.eval()
 
-    # with torch.no_grad():
-    #     for _, test_batch in enumerate(tqdm(data_loader, ncols=80)):
-    #         batch_size = test_batch.shape[0]
-    #         data_test = test_batch.to(device,dtype=torch.double)
-    #         print(type(data_test))
-    #         print('batch size',batch_size)
-    #         print('data test shape ', np.array(data_test).shape)
-    #
-    #         #print("test data0",test_batch[0])      # ([[[[ 4.8549e-10,  0.0000e+00,  1.6389e-08,  ...,  1.3931e-08, 1.4032e-08,  1.3805e-08],
-    #
-    #         #print("test data1", test_batch[1])   # tensor([[[[ 3.6468e-10,  1.4202e-09,  1.4655e-09,  ...,  1.4667e-08,1.4856e-08,  1.4189e-08],
-    #
-    #         # print("test data2", test_batch[2])    # ([[[[ 1.3036e-08,  1.2731e-08,  2.6048e-10,  ...,  1.6858e-08,6.5346e-11,  1.2288e-10],
-    #
-    #         #print("test data3", test_batch[3])      #  tensor([[[[ 0.0000e+00,  1.1671e-08,  0.0000e+00,  ...,  1.7058e-08,1.3300e-10,  1.6663e-08],
-    #
-    #         N, D,C, H, W  = data_test.shape
-    #         print(f' N{N}, D{D}, H{H}, W{W} ,C{C} ')
-    #         data_test = data_test.view(N * D,C, H, W)
-    #
-    #         data_test = data_test[:(N * D) // base_len * base_len]    # data_test -- [840,6,72,72]
-    #         #pred_ppg_test = model(data_test)
-    #
-    #         for idx in range(batch_size):
-    #
-    #             subj_index = test_batch[2][idx]
-    #             #print("\n subj_index: \n ", subj_index)
-    #
-    #             sort_index = int(test_batch[3][idx])
-    #             #print("\n this is sort_index:  \n", sort_index)
-    #
-    #             if subj_index not in predictions.keys():
-    #                 predictions[subj_index] = dict()  # making dict of predictions
-    #                 # labels[subj_index] = dict()
-    #             predictions[subj_index][sort_index] = pred_ppg_test[idx * self.chunk_len:(idx + 1) * self.chunk_len]
-    #
 
-####################################################
+
     with torch.no_grad():
         for data_dict in tqdm(data_loader, ncols=80):
             video_name = data_dict['video_name']
@@ -186,43 +146,11 @@ def prediction(data_loader, model_path='A:/Pycharm/newfinaldemo/PURE_TSCAN.pth',
 
     calculate_metrics(predictions)
 
-    ######################################################
-
-
-
-            # for idx in range(batch_size):
-            #     subj_index = test_batch[0][idx]
-            #     #sort_index = int(test_batch[1][idx])
-            #     #sort_index = int(test_batch[1][idx].item()) if test_batch[1][idx].numel() == 1 else test_batch[1][idx].tolist()
-            #     # sort_index = tuple(test_batch[1][idx].tolist()) if isinstance(test_batch[1][idx], list) else test_batch[1][idx].item()
-            #     if test_batch[1][idx].numel() == 1:
-            #         sort_index = test_batch[1][idx].item()
-            #     else:
-            #         sort_index = tuple(test_batch[1][idx].tolist())
-            #
-            #     if subj_index not in predictions.keys():
-            #         predictions[subj_index] = dict()
-            #
-            #
-            #
-            #     if subj_index not in predictions.keys():
-            #         predictions[subj_index] = dict()
-            #         predictions[subj_index][sort_index] = pred_ppg_test[idx * chunk_len:(idx + 1) * chunk_len]
-
-    #print('\n this is predictions \n', len(predictions))
-
-#    calculate_metrics(predictions)
-
-
-
-
-
 
 if __name__ == "__main__":
     input_video = "A:/final_project/test/s23/vid_s23_T1.mp4"
     input_dir = os.path.join(input_video)
     frames= read_video(input_dir)
-    #print(len(frames))
 
  # face processing
     face_processing = FacePreprocessing()
@@ -231,30 +159,21 @@ if __name__ == "__main__":
     print("\nprocessed frames after MP\n",list(processed_frames.shape))
 # frames after processing --> retun noramlize , chunk
     final_frames = process(processed_frames)
-    #print("\nfinal frames\n", final_frames)
-    # print("\n shape of final\n", final_frames.shape)
-    #print("\nchunks:\n", chunks)
-    #print("\n these are chunks\n", list(chunks.shape))
+
 # required as tesnor and  to fit in NDCHW data type mode
 
     final_data = np.float32(final_frames)
 
     transposed_dim= np.transpose(final_data, (0, 1, 4, 2, 3))
-
-
-
     print('after transpose ',transposed_dim.shape)
     test_data = torch.tensor(transposed_dim)
-###############################################
+
     data_dict = {
         'frames': torch.tensor(transposed_dim),
         'video_name': 's23_T1',
         'batch_number': [0,1,2,3]  # Assuming there's only one batch
     }
     data_loader = [data_dict]
-##########################
 
     predict_data = prediction(data_loader)
-    #predit_data= prediction(test_data)
-
     pass
